ccc219d2ab9d21d639808f9189ff73f1e04a0190
Ashutosh-360 <ashutoshnaik360@gmail.com>
2022-10-11 17:34:56 +0530

    df

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import {BrowserRouter as Router ,Routes,Route} from "react-router-dom";
+import Header from './components/Header/Header';
+import Home from './pages/home/Home';
+
+function App() {
+  return (
+    <div className="App">
+    <Router>
+    <Header/>
+      <Routes>
+        <Route index element={<Home/>}></Route>
+        <Route path='movie/:id' element={<h1>Movie detail page</h1>}></Route>
+        <Route path='movies/:type' element={<h1>List page</h1>}></Route>
+        <Route path='/*' element={<h1>error page</h1>}></Route>
+      </Routes>
+    </Router>
+      
+    </div>
+  );
+}
+
+export default App;
